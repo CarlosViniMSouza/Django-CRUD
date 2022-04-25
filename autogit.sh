eb9ce75a271ae202b7586f4cdf6c50d15ef1feb6
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-04-24 23:04:43 -0300

    project open in WSL1 with Debian - Next Step: create a project in WSL (and used only this enviroment/exit of Windows 11)

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "errors in settings.py | Django_CRUD/urls.py and book_api/urls.py"
+git commit -m "project open in WSL1 with Debian - Next Step: create a project in WSL (and used only this enviroment/exit of Windows 11)"
 git push
 git status
